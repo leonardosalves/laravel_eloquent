382cccb0dc5e601f267532abd7fc20d9a7949fe7
leonardo <ti.leonardo@restauramundo.com>
2017-06-25 13:27:36 -0300

    Scopes

--- app/Post.php
+++ app/Post.php
@@ -1,16 +1,26 @@
 <?php
 
 namespace App;
-
+use \Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
-
+use \Illuminate\Database\Eloquent\Builder;
 class Post extends Model
 {
     //
     protected $fillable = ['title','body','published_at'];
     //protected $guarded = ['id','created_at','updated_at'];
-    protected $dates = ['created_at','updated_at','published_at'];
+    protected $dates = ['published_at'];
 
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope('published', function(Builder $builder){
+            $builder->where('published_at','<', Carbon::now()->format('Y-m-d H:i:s'));
+        });
+    }
+
+    /*
     public function setTitleAttribute($value)
     {
         $this->attributes['title'] = strtolower($value);
@@ -20,4 +30,12 @@ class Post extends Model
     {
         return ucfirst($value);
     }
+    */
+
+    public function scopeOfType($query, $type)
+    {
+        return $query->where('type',$type);
+    }
+
+
 }
