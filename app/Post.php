7eacccb123bdc370b0ec4fd7539402ab1196a162
leonardo <ti.leonardo@restauramundo.com>
2017-06-25 12:59:04 -0300

    Convenções

--- app/Post.php
+++ app/Post.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     //
-    protected $fillable = ['title','body'];
+    protected $fillable = ['title','body','published_at'];
     //protected $guarded = ['id','created_at','updated_at'];
+    protected $dates = ['created_at','updated_at','published_at'];
 }
--- database/migrations/2017_06_25_150720_create_posts_table.php
+++ database/migrations/2017_06_25_150720_create_posts_table.php
@@ -19,6 +19,7 @@ class CreatePostsTable extends Migration
             $table->text('body');
             $table->enum('type',['text', 'image']);
             $table->timestamps();
+            $table->timestamp('published_at');
         });
     }
 
