574be3b674e1af0059e5d6eb546e8b22410db61e
leonardo <ti.leonardo@restauramundo.com>
2017-06-25 13:11:26 -0300

    Mutators e Acessors

--- app/Post.php
+++ app/Post.php
@@ -10,4 +10,14 @@ class Post extends Model
     protected $fillable = ['title','body','published_at'];
     //protected $guarded = ['id','created_at','updated_at'];
     protected $dates = ['created_at','updated_at','published_at'];
+
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = strtolower($value);
+    }
+
+    public function getTitleAttribute($value)
+    {
+        return ucfirst($value);
+    }
 }
